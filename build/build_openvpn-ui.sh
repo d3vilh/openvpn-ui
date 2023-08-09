8f683f2764efd40d9c5d44f5f7b8b2a5f44ca46d
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-08-09 16:59:17 +0300

    Updated and tested build process

--- build/build_openvpn-ui.sh
+++ build/build_openvpn-ui.sh
@@ -4,18 +4,18 @@ set -e
 
 PLATFORM="linux/arm64/v8" # amd64 - "linux/amd64", arm32v5 - "linux/arm/v5", arm32v7 - "linux/arm/v7"
 # Set the current directory
-OD=$PWD
+#OD=$PWD
 
 # go to script folder, required for below steps
-CURDIR=${OD}/$(dirname $0)
-cd "${CURDIR}"
+#CURDIR=${OD}/$(dirname $0)
+#cd "${CURDIR}"
 
 # Build golang & bee environment
 docker build --platform=$PLATFORM -f Dockerfile-beego -t local/beego-v8 -t local/beego-v8:latest .
 ./openvpn-ui-pack2.sh
 
 # Build OpenVPN-UI image
-PKGFILE=openvpn-ui.tar.gz"
+PKGFILE="openvpn-ui.tar.gz"
 cp -f ../$PKGFILE ./
 
 docker build -t local/openvpn-ui .
