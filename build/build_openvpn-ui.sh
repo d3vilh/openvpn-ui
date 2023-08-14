a982179f8954431af193660921705a4662be0064
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-08-14 19:38:01 +0300

    Build benchmark

--- build/build_openvpn-ui.sh
+++ build/build_openvpn-ui.sh
@@ -28,6 +28,9 @@ case $ARCH in
     ;;
 esac
 
+# Benchmarking the start time get
+start_time=$(date +%s)
+
 printf "\033[1;34mBuilding for\033[0m $ARCH ($PLATFORM) with: \n  \033[1;34mUI Image:\033[0m $UIIMAGE \n  \033[1;34mBeeGo Image:\033[0m $BEEIMAGE \n"
 # Update Dockerfile based on platform
 sed -i "s#FROM DEFINE-YOUR-ARCH#$UIIMAGE#g" Dockerfile
@@ -48,3 +51,15 @@ cp -f ../$PKGFILE ./
 docker build -t local/openvpn-ui .
 rm -f $PKGFILE; rm -f $(basename $PKGFILE)
 printf "\033[1;34mAll done.\033[0m\n"
+# Benchmarking the end time record
+end_time=$(date +%s)
+
+# Calculate the execution time in seconds
+execution_time=$((end_time - start_time))
+
+# Calculate the execution time in minutes and seconds
+minutes=$((execution_time / 60))
+seconds=$((execution_time % 60))
+
+# Print the execution time in mm:ss format
+printf "\033[1;34mExecution time: %02d:%02d\033[0m\n" $minutes $seconds
