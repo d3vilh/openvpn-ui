5a5a663c4b6958eb3f262a57f1b75445b43190df
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-08-30 14:29:08 +0300

    remove-pki and saving vars different way

--- build/assets/remove-pki.sh
+++ build/assets/remove-pki.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
 set -e
-
 echo 'Removing PKI.'
-echo 'your PKI has been removed!' > removed.pki.txt
+rm -rf /usr/share/easy-rsa/pki/*
+echo 'PKI removed.'
+echo 'Removing certificates.'
+rm -rf /etc/openvpn/clients/*.ovpn
+echo 'Removing static clients.'
+rm -rf /etc/openvpn/staticclients/*
+echo 'Removing Openvpn-UI DB.'
+rm -rf /etc/openvpn/db/data.db
 echo 'Done' 
