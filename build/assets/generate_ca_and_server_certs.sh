92476fff59115b253c441ebaa57a1500ede2c4a4
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-08-31 11:46:54 +0300

    generate new CA

--- build/assets/generate_ca_and_server_certs.sh
+++ build/assets/generate_ca_and_server_certs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
-
 EASY_RSA=/usr/share/easy-rsa
+PKI_DIR=/tmp/pki
+mkdir -p $PKI_DIR
 
 if [[ ! -f /etc/openvpn/pki/ca.crt ]]; then
     export EASYRSA_BATCH=1 # see https://superuser.com/questions/1331293/easy-rsa-v3-execute-build-ca-and-gen-req-silently
     cd $EASY_RSA
  
     # Copy easy-rsa variables
-    cp /etc/openvpn/config/easy-rsa.vars ./vars
+    cp /etc/openvpn/config/easy-rsa.vars ./pki/vars
 
     # Listing env parameters:
     echo "Following EASYRSA variables will be used:"
@@ -15,7 +16,10 @@ if [[ ! -f /etc/openvpn/pki/ca.crt ]]; then
 
     # Building the CA
     echo 'Setting up public key infrastructure...'
-    $EASY_RSA/easyrsa init-pki
+    $EASY_RSA/easyrsa --pki-dir=$PKI_DIR init-pki
+
+    echo 'Moving PKI directory...'
+    mv $PKI_DIR/* ./pki/
 
     echo 'Generating ertificate authority...'
     $EASY_RSA/easyrsa build-ca nopass
@@ -30,17 +34,21 @@ if [[ ! -f /etc/openvpn/pki/ca.crt ]]; then
     echo 'Generate Diffie-Hellman key...'
     $EASY_RSA/easyrsa gen-dh
 
+    # Generate HMAC signature in "openvpn" container with Docker API to strengthen server certificate.
     echo 'Generate HMAC signature...'
-    openvpn --genkey --secret pki/ta.key
+    nohup sh -c 'curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d '\''{  "Cmd": ["openvpn", "--genkey", "--secret", "/etc/openvpn/pki/ta.key"]}'\'' -X POST http://$DOCKER_HOST_IP/containers/$CONTAINER_ID/exec | jq -r '\''{.Id}'\'' | xargs curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d '\''{"Detach": false, "Tty": false}'\'' -X POST http://$DOCKER_HOST_IP/exec/{}/start' > /dev/null 2>&1 &
+
+    # curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d '{  "Cmd": ["openvpn", "--genkey", "--secret", "/etc/openvpn/pki/ta.key"] }' -X POST http://$DOCKER_HOST_IP/containers/$CONTAINER_ID/exec | jq -r '.Id' | xargs curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d '{"Detach": false, "Tty": false}' -X POST http://$DOCKER_HOST_IP/exec/{}/start
 
     echo 'Create certificate revocation list (CRL)...'
     $EASY_RSA/easyrsa gen-crl
     chmod +r ./pki/crl.pem
 
+    echo 'All done.'
     # Copy to mounted volume
-    cp -r ./pki/. /etc/openvpn/pki
+    # cp -r ./pki/. /etc/openvpn/pki
 else
     # Copy from mounted volume
-    cp -r /etc/openvpn/pki /opt/app/easy-rsa
+    # cp -r /etc/openvpn/pki /opt/app/easy-rsa
     echo 'PKI already set up.'
 fi
--- build/assets/remove-pki.sh
+++ build/assets/remove-pki.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
 set -e
+
+# Variables
+PKI_DIR=/usr/share/easy-rsa/pki
+CERT_DIR=/etc/openvpn/clients
+STATIC_CLIENT_DIR=/etc/openvpn/staticclients
+OVPN_DB_DIR=/etc/openvpn/db
+
+# Remove PKI
 echo 'Removing PKI.'
-rm -rf /usr/share/easy-rsa/pki/*
-echo 'PKI removed.'
-echo 'Removing certificates.'
-rm -rf /etc/openvpn/clients/*.ovpn
-echo 'Removing static clients.'
-rm -rf /etc/openvpn/staticclients/*
-echo 'Removing Openvpn-UI DB.'
-rm -rf /etc/openvpn/db/data.db
-echo 'Done' 
+rm -rf $PKI_DIR/*
+
+# Remove certificates
+echo -e 'PKI removed.\nRemoving certificates.\n'
+rm -rf $CERT_DIR/*.ovpn
+
+# Remove static clients
+echo -e 'Certificates removed.\nRemoving static clients.\n'
+rm -rf $STATIC_CLIENT_DIR/*
+
+# Remove Openvpn-UI DB
+echo -e 'Static clients remover.\nRemoving Openvpn-UI DB.\n'
+rm -rf $OVPN_DB_DIR/data.db
+
+echo 'Done. PKI, Certificates, Satic Clients and OpenVPN-UI DB removed.' 
