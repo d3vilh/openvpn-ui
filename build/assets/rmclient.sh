7448060c65a6f7d74499d146223bc5e1fb776168
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-09-09 20:26:31 +0300

    running revoke based on number certs in store

--- build/assets/rmclient.sh
+++ build/assets/rmclient.sh
@@ -4,6 +4,7 @@ set -e
 
 NAME=$1
 SERIAL=$2
+#ACTION=$3
 
 # .ovpn file path
 DEST_FILE_PATH="/etc/openvpn/clients/$NAME.ovpn"
@@ -14,24 +15,34 @@ if [[ ! -f $DEST_FILE_PATH ]]; then
     exit 1
 fi
 
-# Fix index.txt by removing everything after pattern "/name=$1" in the line
-# Fix for https://github.com/d3vilh/openvpn-ui/issues/5 by shuricksumy@github
-#sed -i'.bak' "s/\/name=${name}\/.*//" /usr/share/easy-rsa/pki/index.txt
-
-# removing the end of the line starting from /name=$NAME for the line that matches the $serial pattern
-sed  -i'.bak' "/$SERIAL/s/\/name=$NAME.*//" /usr/share/easy-rsa/pki/index.txt
-
 export EASYRSA_BATCH=1 # see https://superuser.com/questions/1331293/easy-rsa-v3-execute-build-ca-and-gen-req-silently
 
-echo 'Revoke certificate...'
+# Check if the user has two certificates in index.txt
+if [[ $(cat /usr/share/easy-rsa/pki/index.txt | grep -c "/name=$NAME") -eq 2 ]]; then
+    echo "Revoking renewed certificate..."
+    # removing the end of the line starting from /name=$NAME for the line that matches the $serial pattern
+    sed  -i'.bak' "/$SERIAL/s/\/name=$NAME.*//" /usr/share/easy-rsa/pki/index.txt
+    cd /usr/share/easy-rsa
+    # Revoke renewed certificate
+    ./easyrsa revoke-renewed "$NAME"
+else
+    echo "Revoking certificate..."
+    # removing the end of the line starting from /name=$NAME for the line that matches the $serial pattern
+    sed  -i'.bak' "/$SERIAL/s/\/name=$NAME.*//" /usr/share/easy-rsa/pki/index.txt
+    cd /usr/share/easy-rsa
+    # Revoke certificate
+    ./easyrsa revoke "$NAME"
 
-cd /usr/share/easy-rsa
-# Revoke certificate
-./easyrsa revoke "$NAME"
-
-echo 'Create new Create certificate revocation list (CRL)...'
-./easyrsa gen-crl
-chmod +r ./pki/crl.pem
+    echo 'Create new Create certificate revocation list (CRL)...'
+    ./easyrsa gen-crl
+    chmod +r ./pki/crl.pem
+fi
 
 echo 'Done!'
 echo 'If you want to disconnect the user please restart the service using docker-compose restart openvpn.'
+
+
+# Old Revoke:
+# Fix index.txt by removing everything after pattern "/name=$1" in the line
+# Fix for https://github.com/d3vilh/openvpn-ui/issues/5 by shuricksumy@github
+#sed -i'.bak' "s/\/name=${name}\/.*//" /usr/share/easy-rsa/pki/index.txt
